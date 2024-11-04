25769af3063147ce8fb901208dc6086532f62487
Robert Lillack <rob@lillack.net>
2024-11-04 23:23:51 +0100

    html: Improved parsing of basic entities.

--- html/entities.go
+++ html/entities.go
@@ -1,7 +1,6 @@
 package html
 
 import (
-	"regexp"
 	"strings"
 )
 
@@ -11,58 +10,14 @@ import (
 // Using this to mark breaking spaces, we're able to stay compatible with HTML5.
 const NonCollapsibleSpaceEntity = "&emsp14;"
 
-const LessThanSymbolEntity = "&lt;"
-
 func decodeEntities(s string) string {
 	s = strings.ReplaceAll(s, NonCollapsibleSpaceEntity, " ")
-	s = strings.ReplaceAll(s, LessThanSymbolEntity, "<")
-	return s
-}
-
-func replaceSpaces(s string) string {
-	b := strings.Builder{}
-	for i := 0; i < len(s); i++ {
-		b.WriteString(NonCollapsibleSpaceEntity)
-	}
-	return b.String()
-}
-
-func replaceLeadingSpaces(s string) string {
-	b := strings.Builder{}
-	for i := 0; i < len(s)-1; i++ {
-		b.WriteString(NonCollapsibleSpaceEntity)
-	}
-	b.WriteString(s[len(s)-1:])
-	return b.String()
-}
-
-func replaceTrailingSpaces(s string) string {
-	b := strings.Builder{}
-	b.WriteString(s[0:1])
-	for i := 0; i < len(s)-1; i++ {
-		b.WriteString(NonCollapsibleSpaceEntity)
-	}
-	return b.String()
-}
-
-var multipleSpaces = regexp.MustCompile(`  +`)
-var trailingSpaces = regexp.MustCompile(`\s +`)
-var leadingSpaces = regexp.MustCompile(` +\s`)
-var spacesAtStart = regexp.MustCompile(`^ +`)
-var spacesAtEnd = regexp.MustCompile(` +$`)
-
-func encodeEntities(s string, first, last bool) string {
-	if first {
-		s = spacesAtStart.ReplaceAllStringFunc(s, replaceSpaces)
-	}
-	if last {
-		s = spacesAtEnd.ReplaceAllStringFunc(s, replaceSpaces)
-	}
-	s = multipleSpaces.ReplaceAllStringFunc(s, replaceSpaces)
-	s = trailingSpaces.ReplaceAllStringFunc(s, replaceTrailingSpaces)
-	s = leadingSpaces.ReplaceAllStringFunc(s, replaceLeadingSpaces)
-
-	s = strings.ReplaceAll(s, "<", LessThanSymbolEntity)
+	s = strings.ReplaceAll(s, "&nbsp;", " ")
+	s = strings.ReplaceAll(s, "&lt;", "<")
+	s = strings.ReplaceAll(s, "&gt;", ">")
+	s = strings.ReplaceAll(s, "&amp;", "&")
+	s = strings.ReplaceAll(s, "&quot;", "\"")
+	s = strings.ReplaceAll(s, "&apos;", "'")
 
 	return s
 }
