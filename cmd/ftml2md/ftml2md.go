85d3f85b785b538834e9cb2005aa24b178bb6234
Robert Lillack <rob@lillack.net>
2024-11-06 16:02:37 +0100

    cmd/ftml2md: Allow skipping arguments (or specifying -) for stdin/stdout.

--- cmd/ftml2md/ftml2md.go
+++ cmd/ftml2md/ftml2md.go
@@ -13,18 +13,48 @@ func Errorf(layout string, args ...interface{}) {
 	os.Exit(1)
 }
 
-func main() {
-	if len(os.Args) != 3 {
-		Errorf("Syntax: fmtftml INPUT OUTPUT")
+func mustOpen(fn string) *os.File {
+	if fn == "-" {
+		return os.Stdin
 	}
 
-	inputFile := os.Args[1]
-	outputFile := os.Args[2]
-
-	f, err := os.Open(inputFile)
+	f, err := os.Open(fn)
 	if err != nil {
-		Errorf("Unable to read %s: %s", inputFile, err)
+		Errorf("Unable to open %s for reading: %s", fn, err)
 	}
+	return f
+}
+
+func mustCreate(fn string) *os.File {
+	if fn == "-" {
+		return os.Stdout
+	}
+
+	f, err := os.Create(fn)
+	if err != nil {
+		Errorf("Unable to open %s for writing: %s", fn, err)
+	}
+	return f
+}
+
+func main() {
+	inputFile := "-"
+	outputFile := "-"
+
+	switch len(os.Args) {
+	case 3:
+		outputFile = os.Args[2]
+		fallthrough
+	case 2:
+		inputFile = os.Args[1]
+	case 1:
+		// use defaults
+	default:
+		Errorf("Syntax: fmtftml [INPUT] [OUTPUT]")
+		return
+	}
+
+	f := mustOpen(inputFile)
 	defer f.Close()
 
 	doc, err := ftml.Parse(f)
@@ -35,10 +65,7 @@ func main() {
 		Errorf("Unable to close %s after reading: %s", inputFile, err)
 	}
 
-	f, err = os.Create(outputFile)
-	if err != nil {
-		Errorf("Unable to open %s for writing: %s", outputFile, err)
-	}
+	f = mustCreate(outputFile)
 	if err := markdown.Write(f, doc); err != nil {
 		Errorf("Unable to write document to %s: %s", outputFile, err)
 	}
